197a0fe2cac178f4a60c91e668f028f903fa3b57
akulenbekov <akulenbekov@akchabulak.kg>
2024-06-15 12:18:49 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
+import Advantages from "./components/advantages/advantages";
+import {ADVANTAGES_DATA} from "./constants/Advantages";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className='advantage-flex'>
+            {ADVANTAGES_DATA.map((item) => {
+                return (
+                    <div
+                        key={item.id}>
+                        <Advantages
+                            icon={item.icon}
+                            text={item.text}
+                        />
+                    </div>
+                )
+            })}
+        </div>
+    );
+};
 
 export default App;
---
+++ src/constants/Advantages.js
@@ -0,0 +1,27 @@
+import study from '../assets/icons/icon_study.svg'
+import experience from '../assets/icons/25+.svg'
+import wheel from '../assets/icons/icon_wheel.svg'
+import time from '../assets/icons/icon_time.svg'
+
+export const ADVANTAGES_DATA = [
+    {
+        id: 1,
+        text: '1000+ довольных клиентов',
+        icon: study
+    },
+    {
+        id: 2,
+        text: 'Более 25 лет стаж работы',
+        icon: experience
+    },
+    {
+        id: 3,
+        text: '1.5 Часа - время одного занятия',
+        icon: wheel
+    },
+    {
+        id: 4,
+        text: 'Занятия в удобное для вас время',
+        icon: time
+    },
+]
